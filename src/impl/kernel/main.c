18605d108e4ccc8f9a1d98ad5a9b301802e50833
Voidy Devleoper <77110462+VoidyCD@users.noreply.github.com>
2021-12-17 08:22:22 +0300

    added some new text for info

--- src/impl/kernel/main.c
+++ src/impl/kernel/main.c
@@ -5,6 +5,6 @@ void kernel_main()
     print_clear();
     print_set_color(PRINT_COLOR_YELLOW, PRINT_COLOR_BLACK);
     print_str("Welcome to our 64-bit Dodo OS");
-    print_set_color(PRINT_COLOR_BLUE, PRINT_COLOR_BLACK);
-    print_str("This a boot version. i will be adding more stuff soon");
+    print_set_color(PRINT_COLOR_WHITE, PRINT_COLOR_BLACK);
+    print_str("\nThis a boot version. i will be adding more stuff soon");
 }
